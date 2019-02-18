1c53a03e3dcf6978087078d450c63533080e2fe4
Your Name <you@example.com>
2019-02-18 15:43:34 +0800

    first

---
+++ test/src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="header">
+		<router-link to="/">
+			<div class="iconfont icon-fanhui header-back"></div>
+		</router-link>
+		城市选择
+	</div>
+	
+</template>
+<script>
+export default {
+	name:'CityHeader',
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+	position:relative
+	overflow:hidden
+	height:$headerHeight
+	line-height:$headerHeight
+	text-align:center
+	color:#fff
+	background:$bgColor
+	font-size:.32rem
+	.header-back
+		position:absolute
+		left:0
+		top:0
+		width:.64rem
+		text-align:center
+		font-size:.4rem
+		color:#fff
+</style>
---
+++ test/src/pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<div class="iconfont icon-fanhui back-icon"></div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont icon-sousuo"></span>
+		输入景点\城市\游玩主题</div>
+		<router-link to='/city'>
+			<div class="header-right">
+				{{this.city}}
+				<span class="iconfont icon-jiantou arrow-icon"></span>
+			</div>
+		</router-link>	
+	</div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default{
+	name:'HomeHeader',
+	computed: {
+		...mapState(['city'])
+	}
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+@import '~styles/varibles.styl'
+.header
+	line-height:.86rem
+	display:flex
+	background:$bgColor
+	color:#fff
+	.header-left
+		width:.64rem
+		float:left
+		.back-icon
+			text-align:center
+			font-size:.4rem
+	.header-input
+		flex:1
+		height:0.64rem
+		line-height:0.64rem
+		margin-top:.12rem
+		margin-left:.2rem
+		background:#fff
+		border-radius:.1rem
+		color:#ccc
+	.header-right
+		min-width:1.04rem
+		padding:0 .1rem
+		float:right
+		text-align:center
+		color:#fff
+		.arrow-icon
+			margin-left:-.04rem
+			font-size:.24rem
+</style>
